75ff37e9cf60b3f835a9a3e9dbc11566ede08be7
Younes Hashlamoun <Younes.Engineer@Gmail.com>
2022-10-21 19:53:45 -0500

    6-34 sections ready modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 // import { ModalService } from '../services/modal.service';
 @NgModule({
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, TabComponent, TabsContainerComponent],
   imports: [CommonModule],
-  exports: [ModalComponent],
+  exports: [ModalComponent, TabsContainerComponent, TabComponent],
   // providers: [ModalService],
 })
 export class SharedModule {}
