f814eb660e8be9ad7b4e9898ff8444e1df547669
Younes Hashlamoun <Younes.Engineer@Gmail.com>
2022-10-27 19:57:55 -0500

    register form ready

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,27 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 // import { ModalService } from '../services/modal.service';
 @NgModule({
-  declarations: [ModalComponent, TabComponent, TabsContainerComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent],
+  declarations: [
+    ModalComponent,
+    TabComponent,
+    TabsContainerComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskModule.forRoot()],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
   // providers: [ModalService],
 })
 export class SharedModule {}
