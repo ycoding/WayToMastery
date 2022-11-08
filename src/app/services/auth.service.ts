b6e15fca3e4f0eea5b3ed0338c57bcffe8b28bbb
Younes Hashlamoun <Younes.Engineer@Gmail.com>
2022-11-07 21:38:23 -0600

    adding routers and routes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,9 +4,12 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import IUser from '../models/user.model';
-import { map, delay } from 'rxjs/operators';
+import { map, delay, filter, switchMap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,10 +17,25 @@ export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+  private redirect = false;
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
+    this.router.events
+      .pipe(
+        filter((e) => e instanceof NavigationEnd),
+        map((e) => this.route.firstChild),
+        switchMap((route) => route?.data ?? of({}))
+      )
+      .subscribe((data) => {
+        this.redirect = data['authOnly'] ?? false;
+      });
   }
 
   public async createUser(userData: IUser) {
@@ -42,5 +60,15 @@ export class AuthService {
     await userCred.user.updateProfile({
       displayName: userData.name,
     });
+  }
+
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+    await this.auth.signOut();
+    if (this.redirect) {
+      await this.router.navigateByUrl('/');
+    }
   }
 }
